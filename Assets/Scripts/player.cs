68eec4ee518189b2facaa329845ebac1f7daa1bb
Aaditya-Singh78 <aaditya.out@gmail.com>
2022-08-03 23:08:19 +0530

    faet: shooting ability

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -5,6 +5,9 @@ public class player : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 3.5f;
+    private GameObject _laserPrefab;
+    private float _canfire = 0.5f;
+    private float _fireRate = -1f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +18,7 @@ public class player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
@@ -27,8 +31,6 @@ public class player : MonoBehaviour
         {
             transform.position = new Vector3(transform.position.x, -3.8f, 0);
         }
-        // left ---> 10.2
-        // right ---> -10.2
         if(transform.position.x > 10.2f)
         {
             transform.position = new Vector3(-10.2f, transform.position.y, 0);
@@ -37,5 +39,11 @@ public class player : MonoBehaviour
         {
             transform.position = new Vector3(10.2f, transform.position.y, 0);
         }
+if (Input.GetkeyDown(KeyCode.Space && Time.time > _canfire))
+{
+            _canfire = Time.time + _fireRate;
+            Instantiate(_laserPrefab,transform.position + new Vector3(0,0.8f,0),Quaternion.identity);
+            
+}  
     }
 }
